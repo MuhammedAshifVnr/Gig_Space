daae33443d868623f5fca252430a459b3b5e9093
MuhammedAshifVnr <ashifpvnr@gmail.com>
2024-11-21 15:57:42 +0530

    gig 2

--- Gig_Svc/pkg/db/db.go
+++ Gig_Svc/pkg/db/db.go
@@ -14,6 +14,7 @@ import (
 )
 
 func InitializeDB(DB string) (*gorm.DB, *s3.S3) {
+	DB+="gig_svc"
 	db, err := gorm.Open(postgres.Open(DB), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect DB :%v", err)
